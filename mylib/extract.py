d6b2752e105edd6486e45f31166afbaf0f7be308
PeterdeGuzman <pdeguz01@Peters-MacBook-Pro.local>
2024-10-07 19:33:12 -0400

    updating extract scripts

--- mylib/extract.py
+++ mylib/extract.py
@@ -4,10 +4,7 @@ Extracting dataset from CSV hosted online
 
 import requests
 import os
-
-url = "https://s3.amazonaws.com/dl.ncsbe.gov/ENRS/2020_11_03/polling_place_20201103.csv"
-filepath = "data/pollingplaces_2020.csv"
-directory = "data"
+import zipfile
 
 
 def extract(
@@ -24,10 +21,45 @@ def extract(
     return filepath
 
 
+# add filename argument so you can custom name the downloaded file
+
+
+def extract_zip(url, directory):
+    # Creating directory if not present
+    if not os.path.exists(directory):
+        os.makedires(directory)
+
+    # Creating filepath for zipped file
+    zip_filepath = os.path.join(directory, "downloaded_file.zip")
+    # Downloading zipped file
+    with requests.get(url, timeout=5) as r:
+        r.raise_for_status()  # Raise an error for bad responses
+        with open(zip_filepath, "wb") as f:
+            f.write(r.content)
+    # Extracting zip file
+    with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
+        zip_ref.extractall(directory)
+    # removing zipped file after extraction
+    os.remove(zip_filepath)
+
+    return directory
+
+
 if __name__ == "__main__":
     if os.path.exists("/Users/pdeguz01/Documents/git/PeterdeGuzman_Mini6"):
         os.chdir("/Users/pdeguz01/Documents/git/PeterdeGuzman_Mini6")
     else:
         print("Directory does not exist.")
 
-    extract(url, filepath, directory)
+    extract(
+        url="https://s3.amazonaws.com/dl.ncsbe.gov/ENRS/2020_11_03/polling_place_20201103.csv",
+        filepath="data/pollingplaces_2020.csv",
+        directory="data",
+    )
+    extract_zip(
+        url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter32.zip",
+        directory="data",
+    )
+    extract_zip(
+        url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis32.zip", directory="data"
+    )
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -5,14 +5,58 @@ Transform and load with SQlite3 database
 import sqlite3
 import csv
 
-# dataset = "./data/pollingplaces_2020.csv"
 
-# def load_voterreg(dataset):
+def load_voterreg(dataset, year):
+    data = open(dataset, newline="", encoding="utf-16")
+    db_name = "voterreg_"
+    payload = csv.reader(open(dataset, newline="", encoding="utf-16"), delimiter="\t")
+    conn = sqlite3.connect(f"{db_name}{year}.db")
+    c = conn.cursor()
+    # generate new table for the database
+    c.execute(f"DROP TABLE IF EXISTS {db_name}{year}")
+    c.execute(
+        f"""
+            CREATE TABLE {db_name}{year} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            election_dt DATE,
+            county_name TEXT,
+            polling_place_id INTEGER,
+            polling_place_name TEXT,
+            precinct_name TEXT,
+            house_num INTEGER,
+            street_name TEXT,
+            city TEXT,
+            state TEXT,
+            zip TEXT)
+            """
+    )
+    # insert values
+    c.executemany(
+        f"""
+            INSERT INTO {db_name}{year} (
+            election_dt,
+            county_name,
+            polling_place_id,
+            polling_place_name,
+            precinct_name,
+            house_num,
+            street_name,
+            city,
+            state,
+            zip)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+        payload,
+    )
+    conn.commit()
+    conn.close()
+    return f"{db_name}{year}.db"
+
 
 # def load_votehistory(dataset):
 
 
-def load_pollingplaces(dataset="./data/pollingplaces_2020.csv", year=2020):
+def load_pollingplaces(dataset, year):
     data = open(dataset, newline="", encoding="utf-16")
     # NCSBE data includes null bytes, which must be removed
     payload = csv.reader((line.replace("\0", "") for line in data), delimiter="\t")
@@ -20,10 +64,10 @@ def load_pollingplaces(dataset="./data/pollingplaces_2020.csv", year=2020):
     conn = sqlite3.connect(f"{db_name}{year}.db")
     c = conn.cursor()
     # generate new table for the database
-    c.execute("DROP TABLE IF EXISTS pollingplaces_2020")
+    c.execute(f"DROP TABLE IF EXISTS {db_name}{year}")
     c.execute(
-        """
-            CREATE TABLE pollingplaces_2020 (
+        f"""
+            CREATE TABLE {db_name}{year} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             election_dt DATE,
             county_name TEXT, 
@@ -39,8 +83,8 @@ def load_pollingplaces(dataset="./data/pollingplaces_2020.csv", year=2020):
     )
     # insert values
     c.executemany(
-        """
-            INSERT INTO pollingplaces_2020 (
+        f"""
+            INSERT INTO {db_name}{year} (
             election_dt, 
             county_name,
             polling_place_id,
@@ -57,8 +101,8 @@ def load_pollingplaces(dataset="./data/pollingplaces_2020.csv", year=2020):
     )
     conn.commit()
     conn.close()
-    return "pollingplaces_2020.db"
+    return f"{db_name}{year}.db"
 
 
 if __name__ == "__main__":
-    load_pollingplaces()
+    load_pollingplaces(dataset="./data/pollingplaces_2020.csv", year=2020)
