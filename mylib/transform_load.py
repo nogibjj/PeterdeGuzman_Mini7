bdabf36b11343e5494bd6eabd30cbae4fcc73c96
PeterdeGuzman <pdeguz01@peters-mbp.lan>
2024-10-11 17:06:56 -0400

    updating transform and load to databricks

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -2,11 +2,12 @@
 Transform and load with SQlite3 database
 """
 
-import sqlite3
+from databricks import sql
 import csv
 import pandas as pd
 import os
 from datetime import datetime
+from dotenv import load_dotenv
 
 csv.field_size_limit(100000000)
 
@@ -65,172 +66,104 @@ def transform_votehistory(txtfile, county, date, directory):
 
 
 def load_voterreg(netid, dataset, date):
+    """Loads data into the databricks database"""
     db_name = "voterreg_"
     payload = csv.reader(
         open(dataset, encoding="utf-16"),
         delimiter="\t",
     )
-
-    conn = sqlite3.connect(f"{netid}{db_name}{date}.db")
-    c = conn.cursor()
-    # generate new table for the database
-    c.execute(f"DROP TABLE IF EXISTS {netid}{db_name}{date}")
-    c.execute(
-        f"""
-            CREATE TABLE {netid}{db_name}{date} (
-            county_id INTEGER,
-            county_desc TEXT,
-            voter_reg_num TEXT,
-            ncid TEXT,
-            last_name TEXT,
-            first_name TEXT,
-            middle_name TEXT,
-            name_suffix_lbl TEXT,
-            status_cd TEXT,
-            voter_status_desc TEXT,
-            reason_cd TEXT,
-            voter_status_reason_desc TEXT,
-            res_street_address TEXT,
-            res_city_desc TEXT,
-            state_cd TEXT,
-            zip_code TEXT,
-            mail_addr1 TEXT,
-            mail_addr2 TEXT,
-            mail_addr3 TEXT,
-            mail_addr4 TEXT,
-            mail_city TEXT,
-            mail_state TEXT,
-            mail_zipcode TEXT,
-            full_phone_number TEXT,
-            confidential_ind TEXT,
-            regisr_dt TEXT,
-            race_code TEXT,
-            ethnic_code TEXT,
-            party_cd TEXT,
-            gender_code TEXT,
-            birth_year INTEGER,
-            age_at_year_end TEXT,
-            birth_state TEXT,
-            drivers_lic TEXT,
-            precinct_abbrv TEXT,
-            precinct_desc TEXT,
-            municipality_abbrv TEXT,
-            municipality_desc TEXT,
-            ward_abbrv TEXT,
-            ward_desc TEXT,
-            cong_dist_abbrv TEXT,
-            super_court_abbrv TEXT,
-            judic_dist_abbrv TEXT,
-            nc_senate_abbrv TEXT,
-            nc_house_abbrv TEXT,
-            county_commiss_abbrv TEXT,
-            county_commiss_desc TEXT,
-            township_abbrv TEXT,
-            township_desc TEXT,
-            school_dist_abbrv TEXT,
-            school_dist_desc TEXT,
-            fire_dist_abbrv TEXT,
-            fire_dist_desc TEXT,
-            water_dist_abbrv TEXT,
-            water_dist_desc TEXT,
-            sewer_dist_abbrv TEXT,
-            sewer_dist_desc TEXT,
-            sanit_dist_abbrv TEXT,
-            sanit_dist_desc TEXT,
-            rescue_dist_abbrv TEXT,
-            rescue_dist_desc TEXT,
-            munic_dist_abbrv TEXT,
-            munic_dist_desc TEXT,
-            dist_1_abbrv TEXT,
-            dist_1_desc TEXT,
-            vtd_abbrv TEXT,
-            vtd_description TEXT)
+    load_dotenv()
+    server_h = os.getenv("sql_server_host")
+    access_token = os.getenv("databricks_api_key")
+    http_path = os.getenv("sql_http")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as conn:
+        c = conn.cursor()
+        # generate new table for the database
+        c.execute(f"SHOW TABLES FROM default LIKE 'ped19*'")
+        result = c.fetchall()
+        if not result:
+            c.execute(
+                f"""
+            CREATE TABLE IF NOT EXISTS{netid}{db_name}{date} (
+            county_id INT,
+            county_desc STRING,
+            voter_reg_num STRING,
+            ncid STRING,
+            last_name STRING,
+            first_name STRING,
+            middle_name STRING,
+            name_suffix_lbl STRING,
+            status_cd STRING,
+            voter_status_desc STRING,
+            reason_cd STRING,
+            voter_status_reason_desc STRING,
+            res_street_address STRING,
+            res_city_desc STRING,
+            state_cd STRING,
+            zip_code STRING,
+            mail_addr1 STRING,
+            mail_addr2 STRING,
+            mail_addr3 STRING,
+            mail_addr4 STRING,
+            mail_city STRING,
+            mail_state STRING,
+            mail_zipcode STRING,
+            full_phone_number STRING,
+            confidential_ind STRING,
+            regisr_dt STRING,
+            race_code STRING,
+            ethnic_code STRING,
+            party_cd STRING,
+            gender_code STRING,
+            birth_year INT,
+            age_at_year_end STRING,
+            birth_state STRING,
+            drivers_lic STRING,
+            precinct_abbrv STRING,
+            precinct_desc STRING,
+            municipality_abbrv STRING,
+            municipality_desc STRING,
+            ward_abbrv STRING,
+            ward_desc STRING,
+            cong_dist_abbrv STRING,
+            super_court_abbrv STRING,
+            judic_dist_abbrv STRING,
+            nc_senate_abbrv STRING,
+            nc_house_abbrv STRING,
+            county_commiss_abbrv STRING,
+            county_commiss_desc STRING,
+            township_abbrv STRING,
+            township_desc STRING,
+            school_dist_abbrv STRING,
+            school_dist_desc STRING,
+            fire_dist_abbrv STRING,
+            fire_dist_desc STRING,
+            water_dist_abbrv STRING,
+            water_dist_desc STRING,
+            sewer_dist_abbrv STRING,
+            sewer_dist_desc STRING,
+            sanit_dist_abbrv STRING,
+            sanit_dist_desc STRING,
+            rescue_dist_abbrv STRING,
+            rescue_dist_desc STRING,
+            munic_dist_abbrv STRING,
+            munic_dist_desc STRING,
+            dist_1_abbrv STRING,
+            dist_1_desc STRING,
+            vtd_abbrv STRING,
+            vtd_description STRING)
             """
-    )
-    # insert values
-    c.executemany(
-        f"""
-            INSERT INTO {netid}{db_name}{date} (
-            county_id,
-            county_desc,
-            voter_reg_num,
-            ncid,
-            last_name,
-            first_name,
-            middle_name,
-            name_suffix_lbl,
-            status_cd,
-            voter_status_desc,
-            reason_cd,
-            voter_status_reason_desc,
-            res_street_address,
-            res_city_desc,
-            state_cd,
-            zip_code,
-            mail_addr1,
-            mail_addr2,
-            mail_addr3,
-            mail_addr4,
-            mail_city,
-            mail_state,
-            mail_zipcode,
-            full_phone_number,
-            confidential_ind,
-            regisr_dt,
-            race_code,
-            ethnic_code,
-            party_cd,
-            gender_code,
-            birth_year,
-            age_at_year_end,
-            birth_state,
-            drivers_lic,
-            precinct_abbrv,
-            precinct_desc,
-            municipality_abbrv,
-            municipality_desc,
-            ward_abbrv,
-            ward_desc,
-            cong_dist_abbrv,
-            super_court_abbrv,
-            judic_dist_abbrv,
-            nc_senate_abbrv,
-            nc_house_abbrv,
-            county_commiss_abbrv,
-            county_commiss_desc,
-            township_abbrv,
-            township_desc,
-            school_dist_abbrv,
-            school_dist_desc,
-            fire_dist_abbrv,
-            fire_dist_desc,
-            water_dist_abbrv,
-            water_dist_desc,
-            sewer_dist_abbrv,
-            sewer_dist_desc,
-            sanit_dist_abbrv,
-            sanit_dist_desc,
-            rescue_dist_abbrv,
-            rescue_dist_desc,
-            munic_dist_abbrv,
-            munic_dist_desc,
-            dist_1_abbrv,
-            dist_1_desc,
-            vtd_abbrv,
-            vtd_description)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?, ?, ?)
-        """,
-        payload,
-    )
-    conn.commit()
-    conn.close()
-    return f"{netid}{db_name}{date}.db"
+            )
+            # insert
+            for _, row in payload.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO {netid}{db_name}{date} VALUES {convert}")
+        c.close()
+    return "success"
 
 
 # update table columns and values
@@ -240,58 +173,46 @@ def load_votehistory(netid, dataset, date):
         open(dataset, encoding="utf-16"),
         delimiter="\t",
     )
-
-    conn = sqlite3.connect(f"{netid}{db_name}{date}.db")
-    c = conn.cursor()
-    # generate new table for the database
-    c.execute(f"DROP TABLE IF EXISTS {netid}{db_name}{date}")
-    c.execute(
-        f"""
+    load_dotenv()
+    server_h = os.getenv("sql_server_host")
+    access_token = os.getenv("databricks_api_key")
+    http_path = os.getenv("sql_http")
+    with sql.connect(
+        server_hostname=server_h,
+        http_path=http_path,
+        access_token=access_token,
+    ) as conn:
+        c = conn.cursor()
+        # generate new table for the database
+        c.execute(f"SHOW TABLES FROM default LIKE '{netid}*'")
+        result = c.fetchall()
+        if not result:
+            c.execute(
+                f"""
             CREATE TABLE {netid}{db_name}{date} (
-            county_id INTEGER,
-            county_desc TEXT,
-            voter_reg_num TEXT,
-            election_lbl TEXT,
-            election_desc TEXT,
-            voting_method TEXT,
-            voted_party_cd TEXT,
-            voted_party_desc TEXT,
-            pct_label TEXT,
-            pct_description TEXT,
-            ncid TEXT,
-            voted_county_id TEXT,
-            voted_county_desc TEXT,
-            vtd_label TEXT,
-            vtd_description TEXT)
+            county_id INT,
+            county_desc STRING,
+            voter_reg_num STRING,
+            election_lbl STRING,
+            election_desc STRING,
+            voting_method STRING,
+            voted_party_cd STRING,
+            voted_party_desc STRING,
+            pct_label STRING,
+            pct_description STRING,
+            ncid STRING,
+            voted_county_id STRING,
+            voted_county_desc STRING,
+            vtd_label STRING,
+            vtd_description STRING)
             """
-    )
-    # insert values
-    c.executemany(
-        f"""
-            INSERT INTO {netid}{db_name}{date} (
-            county_id,
-            county_desc,
-            voter_reg_num,
-            election_lbl,
-            election_desc,
-            voting_method,
-            voted_party_cd,
-            voted_party_desc,
-            pct_label,
-            pct_description,
-            ncid,
-            voted_county_id,
-            voted_county_desc,
-            vtd_label,
-            vtd_description)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, ?)
-        """,
-        payload,
-    )
-    conn.commit()
-    conn.close()
-    return f"{netid}{db_name}{date}.db"
+            )
+            # insert
+            for _, row in payload.iterrows():
+                convert = (_,) + tuple(row)
+                c.execute(f"INSERT INTO {netid}{db_name}{date} VALUES {convert}")
+        c.close()
+    return "success"
 
 
 # def load_pollingplaces(dataset, year):
