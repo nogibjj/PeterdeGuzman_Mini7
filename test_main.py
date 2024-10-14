5dce50f456543d71d5beb50edd80dec4a10e0697
PeterdeGuzman <pdeguz01@peters-mbp.lan>
2024-10-14 14:09:22 -0400

    updating test main

--- test_main.py
+++ test_main.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 from databricks import sql
 import os
 import csv
+from mylib.transform_load import (
+    transform_voterreg,
+    transform_votehistory,
+    trim_dataset,
+)
 
 
 def test_extract_zip():
@@ -90,6 +95,39 @@ def test_load_votehistory():
 
 
 if __name__ == "__main__":
+    extract_zip(
+        url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter32.zip",
+        directory="data",
+    )
+    extract_zip(
+        url="https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis32.zip", directory="data"
+    )
+    main_directory = "/Users/pdeguz01/Documents/git/PeterdeGuzman_Mini6/"
+    os.chdir(main_directory)
+    transform_voterreg(
+        txtfile=f"{main_directory}/data/ncvoter32.txt",
+        county="Durham",
+        date="241011",
+        directory="data",
+    )
+    transform_votehistory(
+        txtfile=f"{main_directory}/data/ncvhis32.txt",
+        county="Durham",
+        date="241011",
+        directory="data",
+    )
+    trim_dataset(
+        dataset=f"{main_directory}/data/voterreg_Durham241011.csv",
+        dataset_type="voterreg",
+        n=5000,
+        directory="data",
+    )
+    trim_dataset(
+        dataset=f"{main_directory}/data/votehist_Durham241011.csv",
+        dataset_type="voterhist",
+        n=5000,
+        directory="data",
+    )
     test_extract_zip()
     test_load_voterreg()
     test_load_votehistory()
